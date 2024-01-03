58942bfa66eb1121341c0d43385f984535317b15
Pablo Díaz <pdiaz9731@gmail.com>
2024-01-02 22:41:27 -0600

    Menu Basico

--- Calculadora1/src/main/java/com/mycompany/calculadora1/Calculadora1.java
+++ Calculadora1/src/main/java/com/mycompany/calculadora1/Calculadora1.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.calculadora1;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,16 @@ package com.mycompany.calculadora1;
 public class Calculadora1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese Valor 1");
+        double num1 = scanner.nextDouble();
+        
+        System.out.println("Ingrese Valor 2");
+        double num2 = scanner.nextDouble();
+        
+        scanner.close();
     }
 }
+//
